bae8f68d715b1698a504a69da859b667de74eb54
Lucas Mendes <lucasespassini@gmail.com>
2023-09-20 17:00:40 -0300

    automatizando tipo relatorio

--- index.js
+++ index.js
@@ -14,11 +14,15 @@ const gerarJson = () => {
     objetos.push({ key, valor });
   }
 
-  const tipoRelatorio = objetos.find(objeto => objeto['key'] === 'tipoRelatorio')
+  let tipoRelatorio = objetos.find(objeto => objeto['key'] === 'tipoRelatorio')
+  if (!tipoRelatorio) {
+    tipoRelatorio = objetos.find(objeto => objeto['key'] === 'dfsTitulo')
+  }
 
+  let data
   switch (tipoRelatorio.valor) {
     case 'veiculo':
-      const data = {
+      data = {
         url: {
           key: "url",
           valor: "https://marte.hinova.com.br/sga/sgav4_astran/relatorio/geraRelatorioVeiculo.php"
@@ -28,10 +32,35 @@ const gerarJson = () => {
           valor: "2"
         }
       }
-      objetos.push(data.url)
-      objetos.push(data.tipo)
+      break
+    case 'associado':
+      data = {
+        url: {
+          key: "url",
+          valor: "https://marte.hinova.com.br/sga/sgav4_astran/relatorio/geraRelatorioAssociado.php"
+        },
+        tipo: {
+          key: "tipo",
+          valor: "2"
+        }
+      }
+      break
+    case 'RELATÓRIO DE BOLETOS':
+      data = {
+        url: {
+          key: "url",
+          valor: "https://marte.hinova.com.br/sga/sgav4_astran/relatorio/geraRelatorioBoleto.php"
+        },
+        tipo: {
+          key: "tipo",
+          valor: "4"
+        }
+      }
       break
   }
+
+  objetos.push(data.url)
+  objetos.push(data.tipo)
 
   const relatorioJSON = JSON.stringify(objetos, null, 2);
   const jsonOutput = document.getElementById("jsonOutput");
